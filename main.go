caa8d00af5aec83b5286f2ce9de69f85ad45e8e3
Tyler <tylerjgaw@gmail.com>
2018-03-19 20:48:36 -0400

    Added signup page

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ func main() {
 	s.Router.Post("/login", Login)
 	s.Router.Post("/register", Register)
 	s.Router.Get("/login", views.RenderLogin)
+	s.Router.Get("/signup", views.RenderSignup)
 
 	s.Run()
 }
--- views/views.go
+++ views/views.go
@@ -6,10 +6,15 @@ import (
 )
 
 var loginTemplate *template.Template
+var signupTemplate *template.Template
 
 func init() {
 	var err error
 	loginTemplate, err = template.ParseFiles("views/templates/login.gohtml")
+	if err != nil {
+		panic(err)
+	}
+	signupTemplate, err = template.ParseFiles("views/templates/signup.gohtml")
 	if err != nil {
 		panic(err)
 	}
@@ -19,6 +24,14 @@ func init() {
 func RenderLogin(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	if err := loginTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
+}
+
+// RenderSignup - renders the signup page
+func RenderSignup(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	if err := signupTemplate.Execute(w, nil); err != nil {
 		panic(err)
 	}
 }
